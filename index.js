35439c913e1864d5a0fa17a95f40bfd9481c6086
Atlas <antonio.claudino@fido.com.vc>
2024-03-12 18:09:22 -0300

    done DESAFIO 1

---
+++ index.js
@@ -0,0 +1,14 @@
+// DESAFIO 01
+function calculateSum() {
+  const index = 13;
+  let = sum = 0;
+  let = k = 0;
+
+  while (k < index) {
+    k = k + 1;
+    sum = sum + k;
+  }
+  console.log(sum);
+  //o valor final de sum será 91
+}
+calculateSum();
